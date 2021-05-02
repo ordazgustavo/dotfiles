a39ef40950df9639508b2bad1ad316053214d9d8
Gustavo <ordazsgustavo@gmail.com>
2021-05-02 00:37:37 -0500

    Set some defaults for terminal buffers

--- config/nvim/lua/custom-auto.lua
+++ config/nvim/lua/custom-auto.lua
@@ -12,3 +12,16 @@ augroup highlight_yank
 augroup END
 ]], false)
 
+-- Useful defaults for terminal windows
+vim.api.nvim_exec([[
+augroup neovim_terminal
+    autocmd!
+    " Enter Terminal-mode (insert) automatically
+    autocmd TermOpen * startinsert
+    " Disables number lines on terminal buffers
+    autocmd TermOpen * :set nonumber norelativenumber
+    " allows you to use Ctrl-c on terminal window
+    autocmd TermOpen * nnoremap <buffer> <C-c> i<C-c>
+augroup END
+]], false)
+
