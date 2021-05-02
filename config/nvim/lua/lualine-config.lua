c34eb02611ca4af8c3e6d58a48a7c3a7dab4153e
Gustavo <ordazsgustavo@gmail.com>
2021-05-02 02:32:09 -0500

    Use lualine builtin lsp diagnostics info

--- config/nvim/lua/lualine-config.lua
+++ config/nvim/lua/lualine-config.lua
@@ -1,49 +1,17 @@
-local function lspError()
-  if not vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
-      local error_count = vim.lsp.diagnostic.get_count(0, 'Error')
-      return ' ' .. error_count
-  else
-      return ''
-  end
-end
-
-local function lspWarning()
-  if not vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
-      local warning_count = vim.lsp.diagnostic.get_count(0, 'Warning')
-	  return ' ' .. warning_count
-  else
-      return ''
-  end
-end
-
-local function lspInfo()
-  if not vim.tbl_isempty(vim.lsp.buf_get_clients(0)) then
-      local info_count = vim.lsp.diagnostic.get_count(0, 'Information')
-		  + vim.lsp.diagnostic.get_count(0, 'Hint')
-	  return ' ' .. info_count
-  else
-      return ''
-  end
-end
-
-require('lualine').setup {
+require'lualine'.setup {
 	options = {
 		theme = 'tokyonight',
 	},
 	sections = {
-	  lualine_a = {'mode'},
-	  lualine_b = {'branch'},
-	  lualine_c = {'filename'},
-	  lualine_x = {
-		{ lspError, color = { fg = '#db4b4b', bg = '#1f2335' } },
-		{ lspWarning, color = { fg = '#e0af68', bg = '#1f2335' } },
-		{ lspInfo, color = { fg = '#1abc9c', bg = '#1f2335' } },
-		'encoding',
-		'fileformat',
-		'filetype'
-	  },
-	  lualine_y = {'progress'},
-	  lualine_z = {'location'}
+		lualine_a = {'mode'},
+		lualine_b = {'branch'},
+		lualine_c = {
+			{ 'diagnostics', sources = { 'nvim_lsp' } },
+			'filename'
+		},
+		lualine_x = {'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
 	},
 	extensions = { 'nvim-tree' },
 }
