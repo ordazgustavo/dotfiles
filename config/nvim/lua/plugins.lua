70bbe53dd677b011b153f2fc457ab4aa0938c9d1
Gustavo <ordazsgustavo@gmail.com>
2021-05-02 00:36:30 -0500

    Show LSP info in lualine

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -67,13 +67,6 @@ require('packer').startup(function (use)
 	use {
 		'hoob3rt/lualine.nvim',
 		requires = {'kyazdani42/nvim-web-devicons', opt = true},
-		config = function()
-			require('lualine').setup {
-				options = {
-					theme = 'tokyonight',
-				}
-			}
-		end
 	}
 
 	-- use "romgrk/barbar.nvim"
