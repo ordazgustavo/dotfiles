65fa648f2f2b4f0c85948bb4f39541849b400e0a
Gustavo <ordazsgustavo@gmail.com>
2021-05-10 23:49:28 -0500

    Enable Treesitter indenting and incremental selection

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -3,10 +3,23 @@ require'nvim-treesitter.configs'.setup {
 	highlight = {
 		enable = true,
 	},
-	-- indent = {
-	-- 	enable = true,
-	-- },
+	indent = {
+		enable = true,
+	},
 	autotag = {
 		enable = true,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 }
+
+vim.o.foldmethod = 'expr'
+vim.o.foldlevelstart = 99
+vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]], false)
