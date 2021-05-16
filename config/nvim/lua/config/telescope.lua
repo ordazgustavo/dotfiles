83f4594344d959aa7481f96908ff2059f8215106
Gustavo <ordazsgustavo@gmail.com>
2021-05-16 16:19:54 -0500

    Put telescope prompt in the top

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -3,17 +3,20 @@ local actions = require'telescope.actions'
 
 telescope.setup {
   defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
+    prompt_position = 'top',
+    prompt_prefix = ' ',
+    sorting_strategy = 'ascending',
+    selection_caret = ' ',
+    entry_prefix = '  ',
+    initial_mode = 'insert',
     layout_strategy = 'flex',
     scroll_strategy = 'cycle',
+    winblend = 20,
     mappings = {
       i = {
-        ["<esc>"] = actions.close,
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
+        ['<esc>'] = actions.close,
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
       },
     }
   },
