7cced2041005f4724b989ba2a9f88415fe6108b2
Gustavo <ordazsgustavo@gmail.com>
2021-06-13 00:25:47 -0500

    Use storm theme version in nvim

--- config/nvim/lua/colorscheme.lua
+++ config/nvim/lua/colorscheme.lua
@@ -1,5 +1,7 @@
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_comments = false
 vim.g.tokyonight_italic_keywords = false
+
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 vim.cmd('colorscheme tokyonight')
